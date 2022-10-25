2bf0bcb51fd427a28424915b391e5c6749ff87c4
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-10-25 11:33:25 +0300

    Add game room, Create prisoners counter, Create createBoxesArray function

--- src/components/game/game-settings/index.tsx
+++ src/components/game/game-settings/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { initialSateInterface } from '../../../store/reducer'
+import { showGameSettings, increasePrisonersQtt, decreasePrisonersQtt } from '../../../store/actions'
 
 const GameSettings: React.FC = function () {
+  const dispatch = useDispatch()
+  const prisonersQtt: number = useSelector((state: initialSateInterface) => state.prisonersQtt)
+
+  const onStart = (): void => {
+    dispatch(showGameSettings(false))
+  }
+  const SMALL_QTT_CHANGE = 1
+  const BIG_QTT_CHANGE = 10
+
+  const onSmallIncrease = (): void => {
+    dispatch(increasePrisonersQtt(SMALL_QTT_CHANGE))
+  }
+  const onBigIncrease = (): void => {
+    dispatch(increasePrisonersQtt(BIG_QTT_CHANGE))
+  }
+  const onSmallDecrease = (): void => {
+    dispatch(decreasePrisonersQtt(SMALL_QTT_CHANGE))
+  }
+  const onBigDecrease = (): void => {
+    dispatch(decreasePrisonersQtt(BIG_QTT_CHANGE))
+  }
+
   return (
     <div>
       <p>Select Prisoners quantity</p>
-      <button>+ 10</button>
-      <button>+ 1</button>
-      <p>0</p>
-      <button>- 1</button>
-      <button>- 10</button>
-      <button>Start</button>
+      <button onClick={onBigIncrease}>+ {BIG_QTT_CHANGE}</button>
+      <button onClick={onSmallIncrease}>+ {SMALL_QTT_CHANGE}</button>
+      <p>{prisonersQtt}</p>
+      <button onClick={onSmallDecrease}>- {SMALL_QTT_CHANGE}</button>
+      <button onClick={onBigDecrease}>- {BIG_QTT_CHANGE}</button>
+      <button onClick={onStart}>Start</button>
     </div>
   )
 }
