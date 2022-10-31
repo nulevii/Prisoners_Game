da8af0106029976425a97f180cd4d9b9a9b6a711
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-31 16:51:02 +0200

    title positioning

--- src/components/main-menu/index.tsx
+++ src/components/main-menu/index.tsx
@@ -6,8 +6,6 @@ import { changeGameStatus, showRules, showAbout } from '../../store/actions'
 import Rules from '../rules'
 import AboutSection from './about-section'
 
-import sprite from './../../img/sprite.png'
-
 const MainMenu: React.FC = () => {
   const dispatch = useDispatch()
   const gameRulseStatus: boolean = useSelector((state: InitialStateInterface) => state.gameRules)
@@ -27,14 +25,15 @@ const MainMenu: React.FC = () => {
   return (
     <div className='menuContainer'>
       <div className='gameTitle'>
-        <div className="TitleLetter TitleLetterP"><img src={sprite} alt="" width="150" /></div>
+        <div className="TitleLetter TitleLetterP"></div>
+        <div className="TitleLetter TitleLetterR"></div>
         <div className="TitleLetter TitleLetterI"></div>
         <div className="TitleLetter TitleLetterS"></div>
         <div className="TitleLetter TitleLetterO"></div>
         <div className="TitleLetter TitleLetterN"></div>
         <div className="TitleLetter TitleLetterE"></div>
-        <div className="TitleLetter TitleLetterR"></div>
-        <div className="TitleLetter TitleLetterS"></div>
+        <div className="TitleLetter TitleLetterRR"></div>
+        <div className="TitleLetter TitleLetterSS"></div>
       </div>
       <button onClick={onStartGame} >Start game</button>
       <button onClick={onInstruction}>Instruction</button>
