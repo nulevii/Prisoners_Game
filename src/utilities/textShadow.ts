29f32652125bc08ab1b7b9e4d90842331be9b12b
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-08 12:52:27 +0200

    text shadow only when close to btns

--- src/utilities/textShadow.ts
+++ src/utilities/textShadow.ts
@@ -11,10 +11,12 @@ export const setTextShadow = function (): void {
         const shadow = { x: x - mouseX + width / 2, y: y - mouseY + height / 2 }
         const xval = Math.abs(shadow.x)
         const yval = Math.abs(shadow.y)
-        element.style.filter = `drop-shadow(0 0 2px #000000) drop-shadow(0 0 3px #000000) drop-shadow(${shadow.x}px ${shadow.y}px ${Math.max(
-              Math.max(xval, yval),
-              1
-            )}px black)`
+        if (element.offsetLeft - mouseX < 100 && element.offsetLeft - mouseX > -100 && element.offsetTop - mouseY < 150 && element.offsetTop - mouseY > -150) {
+          element.style.filter = `drop-shadow(0 0 2px #000000) drop-shadow(0 0 3px #000000) drop-shadow(${shadow.x}px ${shadow.y}px ${Math.max(
+                Math.max(xval, yval),
+                1
+              )}px black)`
+        }
       }, 50))
     }
   })
