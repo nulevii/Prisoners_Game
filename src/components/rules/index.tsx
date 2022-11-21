4199396c10967b3107388bf4c37d35f5c13f3fd9
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-22 01:51:05 +0200

    rules modal

--- src/components/rules/index.tsx
+++ src/components/rules/index.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { showRules } from '../../store/actions'
+
 const Rules: React.FC = function () {
   const dispatch = useDispatch()
   const onHideRules = (): void => {
     dispatch(showRules(false))
   }
+  const rulesText = [
+    'First you do one thing',
+    'Then another thing',
+    'And another thing',
+    'At last, you win'
+  ]
+  const leftArrowRef = useRef<HTMLButtonElement>(null)
+  const rightArrowRef = useRef<HTMLButtonElement>(null)
+  const [idx, setCurrentIdx] = useState(0)
+
+  function swapLeft (this: any): void {
+    setCurrentIdx(idx - 1)
+    if (idx === 1) {
+      leftArrowRef.current?.classList.add('visually-hidden')
+    }
+    if (idx === rulesText.length - 1) {
+      rightArrowRef.current?.classList.remove('visually-hidden')
+    }
+  }
+
+  function swapRight (): void {
+    setCurrentIdx(idx + 1)
+    if (idx === 0) {
+      leftArrowRef.current?.classList.remove('visually-hidden')
+    }
+    if (idx === rulesText.length - 2) {
+      rightArrowRef.current?.classList.add('visually-hidden')
+    }
+  }
+
   return (
-    <div >
-      <p>Game Rules</p>
-      <button onClick={onHideRules}>Hide rules</button>
+    <div className='rulesDiv'>
+      <button className='closeRules' onClick={onHideRules}>X</button>
+      <p className='rulesText'>{rulesText[idx]}</p>
+      <button className='rulesLeft rulesArrow visually-hidden' onClick={swapLeft} ref={leftArrowRef}></button>
+      <button className='rulesRight rulesArrow' onClick={swapRight} id='rulesArrowRight' ref={rightArrowRef}></button>
     </div>
   )
 }
