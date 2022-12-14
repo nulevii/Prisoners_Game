d6cc850cb6165af884a9a77599fceabf13c99dbf
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-12-14 14:12:10 +0200

    Fix grammar and rules style

--- src/components/rules/index.tsx
+++ src/components/rules/index.tsx
@@ -8,10 +8,10 @@ const Rules: React.FC = function () {
     dispatch(showRules(false))
   }
   const rulesText = [
-    'In this game you are the prisoner, one of them. Your tusk is to escape. Guards decide to play with you the game in wich you can not win, at least they think so.',
-    'You with your cellmates should find all the keys from the Main Gates in the boxes that Guards prepared for you. Every box has a number on it and has a number inside it. Both numbers equal to quantyty of prisoners.',
-    'You and your friends will enter in the room one by one and will try to find the nubmer inside the box, that equal to your Prisoner ID, key will be there, I supose you remember your ID, as it`s printed on your uniform. Numbers never change!',
-    'You can only open half of the boxes in the room. If you won`t find your number, better luck next time (next time will never happend). In fact even at least one of your cellmates won`t find his number, the game will be finished.'
+    'In this game you are a prisoner, one of them. Your tusk is to escape. Guards decide to play a game with you in which you can not win, at least they think so.',
+    'You with your cellmates should find all the Keys from the Main Gates in the boxes that Guards prepared for you in the Common Room. Every box has a number on it and has a number inside it. Both numbers equal to the quantity of prisoners.',
+    'Every prisoner will enter the room one by one and will try to find the number inside the box that is equal to your Prisoner ID. The key will be there. I suppose you remember your ID, as it`s printed on your uniform. Numbers never change!',
+    'You can only open half of the boxes in the room. If you don`t find your number, better luck next time (next time will never happen). In fact, if at least one of your cellmates won`t find his number, the game will be finished.'
   ]
 
   const leftArrowRef = useRef<HTMLButtonElement>(null)
