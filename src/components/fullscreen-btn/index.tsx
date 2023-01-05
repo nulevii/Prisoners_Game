99e96308c50359d74675cba601dfeb0623c3a310
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-05 16:56:50 +0200

    fs btn shadow

--- src/components/fullscreen-btn/index.tsx
+++ src/components/fullscreen-btn/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useAddShadow } from '../../utilities/textShadow'
 
 const FullScreenBtn: React.FC = () => {
   const [isFullScreen, setIsFullScreen] = useState(false)
+  const textShadowRefs = useAddShadow()
 
   useEffect(() => {
     document.addEventListener('fullscreenchange', checkFullscreen)
@@ -22,7 +24,7 @@ const FullScreenBtn: React.FC = () => {
     setIsFullScreen(!isFullScreen)
   }
 
-  return (<button type='button' name='fullscreen' className={`fullscreen-btn ${isFullScreen ? 'fullscreen-btn--closed' : ''}`} onClick={toggleFullscreen}></button>)
+  return (<button ref={(el) => { textShadowRefs.current![0] = el! }} type='button' name='fullscreen' className={`fullscreen-btn ${isFullScreen ? 'fullscreen-btn--closed' : ''}`} onClick={toggleFullscreen}></button>)
 }
 
 export default FullScreenBtn
