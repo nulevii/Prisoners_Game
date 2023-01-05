a55c95be00b82b89e564437e1795f62097e043dd
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-05 16:58:48 +0200

    envelope btn shadow

--- src/components/game/game-info-block/envelope/index.tsx
+++ src/components/game/game-info-block/envelope/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useAddShadow } from '../../../../utilities/textShadow'
 
 function Envelope (): JSX.Element {
+  const textShadowRefs = useAddShadow()
+
   return (
-    <button className='envelope'></button>
+    <button className='envelope' ref={(el) => { textShadowRefs.current![0] = el! }}></button>
   )
 }
 
