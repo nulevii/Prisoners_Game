9b923ffbe23e4b335abc9d43d66eda3ad30e52e1
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-14 02:08:54 +0200

    Refactor Game Settings to Redux toolkit

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -4,33 +4,6 @@ import {
   CHANGE_GAME_STATUS, START_GAME, STOP_GAME, SOUND_SWITCH, VOLUME_SWITCH, OPEN_BOX, SHOW_MAIN_MENU_CONFIRM_WINDOW, SHOW_RESET_CONFIRM_WINDOW
 } from './action-types'
 
-export const showGameSettings = (payload: boolean): IShowGameSettings => ({
-  type: SHOW_GAME_SETTINGS,
-  payload
-})
-interface IShowGameSettings {
-  type: typeof SHOW_GAME_SETTINGS
-  payload: boolean
-}
-
-export const showMainMenuConfirmWindow = (payload: boolean): IShowMainMenuConfirmWindow => ({
-  type: SHOW_MAIN_MENU_CONFIRM_WINDOW,
-  payload
-})
-interface IShowMainMenuConfirmWindow {
-  type: typeof SHOW_MAIN_MENU_CONFIRM_WINDOW
-  payload: boolean
-}
-
-export const showResetConfirmWindow = (payload: boolean): IShowResetConfirmWindow => ({
-  type: SHOW_RESET_CONFIRM_WINDOW,
-  payload
-})
-interface IShowResetConfirmWindow {
-  type: typeof SHOW_RESET_CONFIRM_WINDOW
-  payload: boolean
-}
-
 // App logic
 
 export const changeGameStatus = (
@@ -42,24 +15,6 @@ export const changeGameStatus = (
 interface IChangeGameStatus {
   type: typeof CHANGE_GAME_STATUS
   payload: 'started' | 'notStarted' | 'win' | 'lose' | 'paused'
-}
-
-export const changePrisonersQtt = (payload: number): IChangePrisonersQtt => ({
-  type: CHANGE_PRISONERS_QTT,
-  payload
-})
-interface IChangePrisonersQtt {
-  type: typeof CHANGE_PRISONERS_QTT
-  payload: number
-}
-
-export const changeTimeLimit = (payload: number): IChangeTimeLimit => ({
-  type: CHANGE_TIME_LIMIT,
-  payload
-})
-interface IChangeTimeLimit {
-  type: typeof CHANGE_TIME_LIMIT
-  payload: number
 }
 
 export const startGame = (): IStartGame => ({
