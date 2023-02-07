3f6daf2f17fe01e565cc57586a16f341fe2dd921
Inna <inna.forkert@gmail.com>
2023-02-07 04:02:41 +0200

    feat: animate rules
    only works one way

--- src/components/rules/index.tsx
+++ src/components/rules/index.tsx
@@ -9,7 +9,9 @@ const Rules: React.FC = function () {
     dispatch(setShowRules(false))
   }
   const textShadowRefs = useAddShadowLight()
-  const [idx, setCurrentIdx] = useState(0)
+  const [currentIdx, setCurrentIdx] = useState(0)
+  const [zIndex, setZIndex] = useState([1, 2, 3, 4])
+  const [disabledArrows, setDisabledArrows] = useState(false)
 
   const rulesText = [
     'In this game you are a prisoner, one among many. Your task is to escape. Guards want to play a game with you, in which you cannot win. At least, they think so.',
@@ -19,27 +21,110 @@ const Rules: React.FC = function () {
   ]
 
   function swapLeft (): void {
-    setCurrentIdx(prevIdx => prevIdx - 1)
+    if (!disabledArrows) {
+      setDisabledArrows(true)
+      setCurrentIdx((prevIdx) => prevIdx - 1)
+      console.log(currentIdx)
+      setTimeout(() => setZIndex(prev => prev.map(el => {
+        if (el === 1) {
+          setDisabledArrows(false)
+          return 4
+        }
+        setDisabledArrows(false)
+        return el - 1
+      })
+      ), 500)
+    }
   }
 
   function swapRight (): void {
-    setCurrentIdx(prevIdx => prevIdx + 1)
+    if (!disabledArrows) {
+      setDisabledArrows(true)
+      setCurrentIdx((prevIdx) => prevIdx + 1)
+      console.log(currentIdx)
+      setTimeout(() => setZIndex(prev => prev.map(el => {
+        if (el === 4) {
+          setDisabledArrows(false)
+          return 1
+        }
+        setDisabledArrows(false)
+        return el + 1
+      })
+      ), 500)
+    }
   }
 
   return (
     <div className="rulesDiv">
-      <button className="closeBtn" onClick={onHideRules} ref={(el) => { textShadowRefs.current![2] = el! }}></button>
-      <p className="rulesText">{rulesText[idx]}</p>
       <button
-        className={`rulesLeft rulesArrow ${idx === 0 ? 'visually-hidden' : ''}`}
+        className="closeBtn"
+        onClick={onHideRules}
+        ref={(el) => {
+          textShadowRefs.current![2] = el!
+        }}
+      ></button>
+      <p
+        className={
+          currentIdx === 4
+            ? 'animatedText rulesText'
+            : 'rulesText'
+        }
+        key={rulesText[3]}
+        style={{ zIndex: zIndex[0] }}
+
+      >
+        {rulesText[3]}
+      </p>
+      <p
+        className={
+          currentIdx === 3
+            ? 'animatedText rulesText'
+            : 'rulesText'
+        }
+        key={rulesText[2]}
+        style={{ zIndex: zIndex[1] }}
+
+      >
+        {rulesText[2]}
+      </p>
+      <p
+        className={
+          currentIdx === 2
+            ? 'animatedText rulesText'
+            : 'rulesText'
+        }
+        key={rulesText[1]}
+        style={{ zIndex: zIndex[2] }}
+      >
+        {rulesText[1]}
+      </p>
+      <p
+        className={
+          currentIdx === 1
+            ? 'animatedText rulesText'
+            : 'rulesText'
+        }
+        style={{ zIndex: zIndex[3] }}
+        key={rulesText[0]}
+      >
+        {rulesText[0]}
+      </p>
+      <button
+        className={`rulesLeft rulesArrow ${currentIdx === 0 ? 'visually-hidden' : ''}`}
         onClick={swapLeft}
-        ref={(el) => { textShadowRefs.current![0] = el! }}
+        ref={(el) => {
+          textShadowRefs.current![0] = el!
+        }}
       ></button>
       <button
-        className={`rulesRight rulesArrow ${idx === rulesText.length - 1 ? 'visually-hidden' : ''}`}
+        className={`rulesRight rulesArrow ${
+          currentIdx === rulesText.length - 1 ? 'visually-hidden' : ''
+        }`}
         onClick={swapRight}
         id="rulesArrowRight"
-        ref={(el) => { textShadowRefs.current![1] = el! }}
+        ref={(el) => {
+          textShadowRefs.current![1] = el!
+        }}
       ></button>
     </div>
   )
