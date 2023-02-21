27d8d59c83c5ff87197d79db29a8b1e3224d5287
Inna <inna.forkert@gmail.com>
2023-02-21 22:58:16 +0200

    feat: game result markup

--- src/components/game/game-room/game-result/index.tsx
+++ src/components/game/game-room/game-result/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react'
-import { useAppSelector } from '../../../../store/hooks'
+import { useAppDispatch, useAppSelector } from '../../../../store/hooks'
+import { stopGame } from '../../../../store/features/game-logic/gameLogicSlice'
+import { setShowGameSettings } from '../../../../store/features/game-settings/gameSettingsSlice'
+import { setOpenGame } from '../../../../store/features/main-menu/mainMenuSlice'
+import { useAddShadow } from '../../../../utilities/textShadow'
+
 const GameResult: React.FC = function () {
+  const dispatch = useAppDispatch()
+  const textShadowRefs = useAddShadow()
   const gameStatus = useAppSelector((state) => state.gameLogic.gameStatus)
+
+  function openMenu (): void {
+    dispatch(stopGame())
+    dispatch(setShowGameSettings(true))
+    dispatch(setOpenGame(false))
+  }
+
   if (gameStatus === 'win') {
-    return (
-    <div className='game-result'>
-      You win
-    </div>
-    )
+    return (<div className="game-result">
+      <button onClick={openMenu} ref={(el) => { textShadowRefs.current![2] = el! }} className='btn'>Main menu</button>
+    </div>)
   }
 
   if (gameStatus === 'lose') {
-    return (
-    <div className='game-result'>
-      You lose
-    </div>
-    )
+    return (<div className="game-result lost">
+      <button onClick={openMenu} ref={(el) => { textShadowRefs.current![2] = el! }} className='btn'>Main menu</button>
+    </div>)
   }
 
   if (gameStatus === 'paused') {
-    return (
-    <div className='game-result'>
-      Game paused
-    </div>
-    )
+    return (<div className="game-result pause"></div>)
   }
 
   return null
